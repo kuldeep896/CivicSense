7bf5bf069d52f072c46c4010ee5f8e6ab74195b2
Kuldeep rajput <kuldeeprajput893046@gmai.com>
2026-05-03 19:01:49 +0530

    temp save before cleanup

---
+++ backend/src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: djvbieajj,
+  api_key: 281649248635169,
+  api_secret: "4z4prwFaTWEBpAWBjRMqee4Kj80",
+});
+
+module.exports = cloudinary;
